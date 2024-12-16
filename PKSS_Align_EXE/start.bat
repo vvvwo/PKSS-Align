d0ebecfbf6204c756657641f53e46f83cebb8a23
Dr. Chenlei Lv <65271555+vvvwo@users.noreply.github.com>
2024-12-16 15:13:07 +0800

    Delete PKSS_Align_EXE directory

--- PKSS_Align_EXE/start.bat
+++
@@ -1,2 +0,0 @@
-PKSS_Align.exe D:/EXE/Registration/PKSS_Align/Data/airplane_0001_s.ply D:/EXE/Registration/PKSS_Align/Data/airplane_0001_t.ply
-pause()
--- PKSS_Align_EXE/start_Parameter.bat
+++
@@ -1,2 +0,0 @@
-PKSS_Align.exe Data/01f_s.ply Data/01f_t.ply 12 3 3000 1
-pause()
